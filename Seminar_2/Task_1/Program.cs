07e79eb7e04580f693c5753367c512357f220ba6
Anna Sharovatova <funtik_staff@mail.ru>
2022-06-28 22:31:18 +0300

    Task_2

--- Seminar_2/Task_1/Program.cs
+++ Seminar_2/Task_1/Program.cs
@@ -1,6 +1,9 @@
-﻿int TakeLastNum(int Num)
+﻿// Напишите программу, которая принимает на вход трехзначное число
+// и на выходе показывает последнюю цифру этого числа.
+
+int TakeLastNum(int Num)
 {
     return Num % 10; 
 }
 
-Console.WriteLine(TakeLastNum(678));
+Console.WriteLine(TakeLastNum(766));
---
+++ Seminar_2/Task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// 4. Напишите программу, которая принимает на вход число и проверяет,
+//    кратно ли оно одновременно 7 и 23.
+
+void OneOfNum(int num_1, int num_2)
+{       
+    if (num_1 % 7 == 0 && num_1 % 23 == 0)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");        
+    }    
+}
+
+OneOfNum(16);
+OneOfNum(161);
+
