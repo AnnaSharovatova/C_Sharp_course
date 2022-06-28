60b3148fb7f341d49c312cc715d782adc9dcdf43
Anna Sharovatova <funtik_staff@mail.ru>
2022-06-29 02:05:26 +0300

    Task_5

---
+++ Seminar_1/Task_5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает одно число(N), 
+// а на выходе показывает все числа в промежутке от -N до N
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int negative_num = -num;
+
+while (num +1 > negative_num)
+{
+    Console.WriteLine(negative_num);
+    negative_num ++;
+}
