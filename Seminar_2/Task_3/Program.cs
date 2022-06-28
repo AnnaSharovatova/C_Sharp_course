4c5d9229e1a5a31d6c988dcd7b8d3828fc24dca0
Anna Sharovatova <funtik_staff@mail.ru>
2022-06-28 23:31:52 +0300

    Task_3

--- Seminar_2/Task_3/Program.cs
+++ Seminar_2/Task_3/Program.cs
@@ -2,4 +2,33 @@
 //    является ли второе число кратным первому. Если число 2 не кратно числу 1, то
 //    программа выводит остаток от деление.
 
+using System;
 
+namespace _2_2
+{
+    class Program
+    {
+        static int CheckinValues(in int value1, in int value2)
+        {
+            return (value1 % value2);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число ");
+            int value1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число ");
+            int value2 = int.Parse(Console.ReadLine());
+
+            int Result = CheckinValues(in value1, in value2);
+
+            if (Result == 0)
+            {
+                Console.WriteLine("Числа кратные");
+            }
+            else
+            {
+                Console.WriteLine($"Остаток от деления {Result}");
+            }
+        }
+    }
+}
