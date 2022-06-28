53b175dad43d994b26b26a7c501b861dca2c3c50
Anna Sharovatova <funtik_staff@mail.ru>
2022-06-29 02:36:20 +0300

    HW_task_two_stars

---
+++ Seminar_1/Homework/HW_task_two_stars/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая на вход принимает значение, а на выходе показывает обратное значение.
+
+Console.Write("Введите число: ");
+double num = double.Parse(Console.ReadLine());
+
+Console.WriteLine(1 / num);
