50cfb0a0759e576bdd3239771cde197491384c88
Anna Sharovatova <funtik_staff@mail.ru>
2022-07-04 10:33:08 +0300

    Task_2

---
+++ Seminar_4/Task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// : Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5 
+
+int CountNum(int num)
+{
+    string result = num.ToString();
+    return result.Length;
+}
+Console.WriteLine(CountNum(456));
