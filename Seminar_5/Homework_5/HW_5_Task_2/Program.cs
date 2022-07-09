28fc257504e6f9ef9afe1fd34cee994c0d417d0a
Anna Sharovatova <funtik_staff@mail.ru>
2022-07-09 03:53:31 +0300

    Refresh HW_5_Task_2

--- Seminar_5/Homework_5/HW_5_Task_2/Program.cs
+++ Seminar_5/Homework_5/HW_5_Task_2/Program.cs
@@ -6,34 +6,34 @@
 
 void Print(int[] arr)
 {
-int size = arr.Length;
+    int size = arr.Length;
 
-for (int i = 0; i < size; i++)
-{
-Console.Write($"{arr[i]} ");
-}
-Console.WriteLine();
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
 }
 
 int[] MassNums(int size)
 {
-int[] arr = new int[size];
+    int[] arr = new int[size];
 
-for (int i = 0; i < size; i++)
-{
-arr[i] = new Random().Next(1, 100);
-}
-return arr;
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
 }
 
-int SumOddIndexNum(int[]arr)
+int SumOddIndexNum(int[] arr)
 {
     int sum_odd = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if(i%2==1)
+        if (i % 2 == 1)
         {
-            sum_odd+=arr[i];
+            sum_odd += arr[i];
         }
     }
     return sum_odd;
