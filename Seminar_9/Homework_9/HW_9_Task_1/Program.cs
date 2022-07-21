7b34dd0c2e7d4b7e880c39214b6860412fb6fa8f
Anna Sharovatova <funtik_staff@mail.ru>
2022-07-21 18:48:12 +0300

    HW_9_Task_2

--- Seminar_9/Homework_9/HW_9_Task_1/Program.cs
+++ Seminar_9/Homework_9/HW_9_Task_1/Program.cs
@@ -9,7 +9,7 @@ int M = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
-void Sequence(int M, int N)
+void SumOfSequence(int M, int N)
 {
     if (N < M) return;
     Sequence(M, N - 1);
