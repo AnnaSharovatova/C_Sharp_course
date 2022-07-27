0e6cd1c24b8f5e4565cdeb419e5b8564021ca8fe
Anna Sharovatova <funtik_staff@mail.ru>
2022-07-27 21:45:55 +0300

    Task_4 done at home

--- Seminar_8/Task_4/Program.cs
+++ Seminar_8/Task_4/Program.cs
@@ -15,24 +15,78 @@
 
 
 
-int[,] ModMass (int[,] arr)
+void Print(int[,] arr)
 {
-    int count = 0;
-    int row = arr.GetLength(0);
-    int column = arr.GetLength(1);
-    while (count <= row*column)
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
     {
-        for (int i = 0; i < row; i++)
+        for (int j = 0; j < column_size; j++)
         {
-            for (int j = 0; j < column-1; j++)
-            {
-                if (arr[i,j] < arr[i,j+1]) 
-                {
-                    (arr[i,j], arr[i,j+1]) = (arr[i,j+1], arr[i,j]);
-                }
-            }
+            Console.Write($" {arr[i, j]} ");
         }
-        count++;
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }
     }
     return arr;
 }
+
+int[] FindPositionOfMinElement(int[,] arr)
+{
+    int MinElement = arr[0, 0];
+    int[] position = new int[2];
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            if (MinElement > arr[i, j])
+            {
+                MinElement = arr[i, j];
+                position[0] = i;
+                position[1] = j;
+            }
+    }
+    Console.WriteLine(arr[position[0], position[1]]);
+    Console.WriteLine();
+    return position;
+}
+
+void IgnoreRowColumnWithMinElement(int[,] arr, int[] min_position)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+            if(min_position[0] == i || min_position[1] == j) continue;
+            else Console.Write($"{arr[i,j],3}");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите число рядов: ");
+int row = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число колонок: ");
+int column = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+int[,] arr_1 = MassNums(row, column, 1, 101);
+Print(arr_1);
+
+int[] mass = FindPositionOfMinElement(arr_1);
+IgnoreRowColumnWithMinElement(arr_1, mass);
