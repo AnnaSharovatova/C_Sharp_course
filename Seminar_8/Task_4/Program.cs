f291f88ec1ace007626088d2a47a6b57330915ba
Anna Sharovatova <funtik_staff@mail.ru>
2022-07-27 19:23:02 +0300

    Task_4

--- Seminar_8/Task_4/Program.cs
+++ Seminar_8/Task_4/Program.cs
@@ -13,3 +13,26 @@
 // 2 2 6
 // 3 4 7
 
+
+
+int[,] ModMass (int[,] arr)
+{
+    int count = 0;
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    while (count <= row*column)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column-1; j++)
+            {
+                if (arr[i,j] < arr[i,j+1]) 
+                {
+                    (arr[i,j], arr[i,j+1]) = (arr[i,j+1], arr[i,j]);
+                }
+            }
+        }
+        count++;
+    }
+    return arr;
+}
