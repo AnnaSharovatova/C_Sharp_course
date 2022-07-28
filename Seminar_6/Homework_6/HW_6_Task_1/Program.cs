5e2983edfb729dc739f737af618484e141117ebb
Anna Sharovatova <funtik_staff@mail.ru>
2022-07-28 23:19:22 +0300

    HW_6_Task_1 refresh

--- Seminar_6/Homework_6/HW_6_Task_1/Program.cs
+++ Seminar_6/Homework_6/HW_6_Task_1/Program.cs
@@ -2,20 +2,26 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-Console.Write("Введите желаемое количество чисел (через пробел): ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-
-int CountPositivNums(int[] arr)
+internal class Program
 {
-    int count = 0;
-    for (int i = 0; i < arr.Length; i++)
+    private static void Main(string[] args)
     {
-        if (arr[i] > 0)
-        {
-            count += 1;
-        }
-    }
-    return count;
-}
+        Console.Write("Введите желаемое количество чисел (через пробел): ");
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 
-Console.WriteLine($"Кол-во элементов > 0: {CountPositivNums(arr)}");
+        int CountPositivNums(int[] arr)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    count += 1;
+                }
+            }
+            return count;
+        }
+
+        Console.WriteLine($"Кол-во элементов > 0: {CountPositivNums(arr)}");
+    }
+}
